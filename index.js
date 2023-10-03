134afd4b5609e69e0754824a55b549691a61b0bb
GedSargent <120935232+GedSargent@users.noreply.github.com>
2023-10-03 13:51:52 +0100

    Add .gitignore and update index.js

--- index.js
+++ index.js
@@ -2,18 +2,24 @@ const express = require("express");
 const app = express();
 const port = 5000;
 
+// Open the server and listen on port 5000
+app.listen(port, () => {
+  console.log(`Now listening on port ${port}`);
+});
+
+// ------------ ROUTING ------------
+
+// Respond with "index.html" file for requests that hit our root "/"
 app.get("/", (req, res) => {
   res.sendFile("index.html", { root: __dirname });
 });
 
+// Respond with "about.html" file for requests that hit our root "/about"
 app.get("/about", (req, res) => {
   res.sendFile("about.html", { root: __dirname });
 });
 
-app.get("/contact", (req, res) => {
-  res.sendFile("contact.html", { root: __dirname });
-});
-
-app.listen(port, () => {
-  console.log(`Now listening on port ${port}`);
-});
+/*
+  Using the functions above as your guide, can you write the code to 
+  respond with our "contact.html" file for requests that hit the "/contact"?
+*/
